a8f268f5ff44cd50ac25aa772501a04ac88e4191
liubang <it.liubang@gmail.com>
2017-08-23 16:22:36 +0800

    add test script

---
+++ tests/test2.php
@@ -0,0 +1,28 @@
+<?php
+
+$ht = new linger\Hashtable(65535);
+
+$n = 10000;
+
+$start = microtime(true);
+for ($i = 0; $i < $n; $i++) {
+    $ht->set("hello{$i}", "world{$i}");
+}
+
+for ($i = 0; $i < $n; $i++) {
+    $ht->get("hello{$i}");
+}
+
+echo "HashTable:" . (microtime(true) - $start), PHP_EOL;
+
+$arr = [];
+
+$start = microtime(true);
+for ($i = 0; $i < $n; $i++) {
+   $arr["hello{$i}"] = "world{$i}"; 
+}
+
+for ($i = 0; $i < $n; $i++) {
+    $tmp = $arr["hello{$i}"];
+}
+echo "Array:" . (microtime(true) - $start), PHP_EOL;
